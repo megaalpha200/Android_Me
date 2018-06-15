6043d27569332830a65ab350e7f8d06707261edf
Jose A. Alvarado <megaalpha200@gmail.com>
2018-06-14 21:10:50 -0500

    Completed Exercise 2

--- app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
@@ -21,6 +21,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.android.android_me.R;
+import com.example.android.android_me.data.AndroidImageAssets;
 
 // This activity will display a custom Android image composed of three body parts: head, body, and legs
 public class AndroidMeActivity extends AppCompatActivity {
@@ -34,16 +35,26 @@ public class AndroidMeActivity extends AppCompatActivity {
         // Create a new head BodyPartFragment
         BodyPartFragment headFragment = new BodyPartFragment();
 
-        // TODO (4) Set the list of image id's for the head fragment and set the position to the second image in the list
+        // TODO DONE(4) Set the list of image id's for the head fragment and set the position to the second image in the list
+        headFragment.setImageResources(AndroidImageAssets.getHeads());
+        headFragment.setImageIndex(1);
+
+        // TODO DONE(5) Create and display the body and leg BodyPartFragments
+        BodyPartFragment bodyFragment = new BodyPartFragment();
+        bodyFragment.setImageResources(AndroidImageAssets.getBodies());
+        bodyFragment.setImageIndex(1);
+
+        BodyPartFragment legFragment = new BodyPartFragment();
+        legFragment.setImageResources(AndroidImageAssets.getLegs());
+        legFragment.setImageIndex(1);
 
         // Add the fragment to its container using a FragmentManager and a Transaction
         FragmentManager fragmentManager = getSupportFragmentManager();
 
         fragmentManager.beginTransaction()
                 .add(R.id.head_container, headFragment)
+                .add(R.id.body_container, bodyFragment)
+                .add(R.id.leg_container, legFragment)
                 .commit();
-
-        // TODO (5) Create and display the body and leg BodyPartFragments
-
     }
 }
